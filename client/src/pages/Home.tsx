546346e0afd3490dda9500b1c017adb8ad9555c2
doviethungkma <shadyside1995@gmail.com>
2022-09-27 15:00:56 +0700

    Hero video banner

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,12 @@
 // import required modules
+import HeroVideo from "../components/common/HeroVideo";
 
 const Home = () => {
-  return <div className="-z-10 bg-background-color"></div>;
+  return (
+    <div className="-z-10 bg-background-color">
+      <HeroVideo />
+    </div>
+  );
 };
 
 export default Home;
