ced2774e6c5825e01f6d4b4a1bf1e6ffef00ec67
doviethungkma <shadyside1995@gmail.com>
2022-11-23 17:27:25 +0700

    Movie popup, Movie popup slider, responsive movie popup

--- client/src/components/common/Overlay.tsx
+++ client/src/components/common/Overlay.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 
-const Overlay = () => {
+interface IOverlayProps {
+  onClick?: () => void;
+}
+
+const Overlay = (props: IOverlayProps) => {
   return (
-    <div className="w-full h-full absolut top-0 left-0 bg-black opacity-70 -z-10"></div>
+    <div
+      className="w-full h-full absolut top-0 left-0 bg-black opacity-70 -z-10"
+      onClick={props.onClick}
+    ></div>
   );
 };
 
--- client/src/components/layout/Home/MoviePopup.tsx
+++ client/src/components/layout/Home/MoviePopup.tsx
@@ -1,11 +1,16 @@
-import React from "react";
-import { IMovie } from "../../../interfaces/movie";
-import Overlay from "./../../common/Overlay";
+import { AxiosResponse } from "axios";
+import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
+import { SwiperSlide } from "swiper/react";
+import movieApi from "../../../api/movieApi";
+import { IMovie } from "../../../interfaces/movie";
 import { hideMoviePopup } from "../../../redux/features/movieSlice";
 import Button from "../../common/Button";
+import EpisodeCard from "../../common/EpisodeCard";
 import MovieDescription from "../../common/MovieDescription";
 import MovieDetail from "../../common/MovieDetail";
+import MoviePopupSlider from "../../common/MoviePopupSlider";
+import Overlay from "./../../common/Overlay";
 
 interface IMoviePopupProps {
   movie: IMovie;
@@ -13,10 +18,21 @@ interface IMoviePopupProps {
 
 const MoviePopup = (props: IMoviePopupProps) => {
   const dispatch = useDispatch();
+  const [movies, setmovies] = useState<IMovie[]>();
+
+  const getData = async () => {
+    const response: AxiosResponse = await movieApi.getRandomMovie(10);
+    setmovies(response.data.movies);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className="w-screen h-screen fixed z-40">
-      <Overlay />
-      <div className="w-[90%] max-w-[872px] max-h-[90%] h-full absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+      <Overlay onClick={() => dispatch(hideMoviePopup())} />
+      <div className="w-[90%] max-w-[872px] h-full max-h-[90%]  absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 overflow-scroll no-scrollbar bg-background-color">
         <div
           className="close__btn absolute right-4 top-4 w-9 h-9 flex items-center justify-center rounded-full bg-black opacity-50 cursor-pointer z-30"
           onClick={() => dispatch(hideMoviePopup())}
@@ -25,13 +41,13 @@ const MoviePopup = (props: IMoviePopupProps) => {
         </div>
         <div className="relative w-full z-20">
           <img src={props.movie.thumb} alt="" className="w-full" />
-          <div className="w-full absolute -bottom-6 left-10">
+          <div className="w-full absolute -bottom-6 sm:left-8">
             <img
               src={props.movie.nameImage}
               alt=""
-              className="w-[50%] max-w-[288px] left-8 mb-10"
+              className="w-[50%] max-w-[288px] ml-4 sm:ml-0 mb-10"
             />
-            <div className="flex gap-4">
+            <div className="flex gap-4 ml-4 sm:ml-0">
               <Button
                 width="w-[149px]"
                 height="h-[48px]"
@@ -58,13 +74,35 @@ const MoviePopup = (props: IMoviePopupProps) => {
             </div>
           </div>
         </div>
-        <div className="w-full px-8 py-[70px] flex gap-2 bg-background-color">
-          <div className="w-[70%]">
+        <div className="w-full px-4 md:px-8 py-[70px] flex flex-col sm:flex-row gap-8 md:gap-2">
+          <div className=" w-full sm:w-[70%]">
             <MovieDescription movie={props.movie} />
           </div>
-          <div>
+          <div className="w-full sm:w-[30%]">
             <MovieDetail movie={props.movie} />
           </div>
+        </div>
+        <div className="w-full px-4 sm:px-8 ">
+          {props.movie.episodes && props.movie.episodes.length > 1 ? (
+            <MoviePopupSlider title="Danh sách tập">
+              {props.movie.episodes.map((item, index) => (
+                <SwiperSlide>
+                  <EpisodeCard episode={item} type="episode" />
+                </SwiperSlide>
+              ))}
+            </MoviePopupSlider>
+          ) : (
+            <></>
+          )}
+        </div>
+        <div className="w-full px-4 sm:px-8 mt-8">
+          <MoviePopupSlider title="Đề xuất cho bạn">
+            {movies?.map((item, index) => (
+              <SwiperSlide>
+                <EpisodeCard type="movie" movie={item} />
+              </SwiperSlide>
+            ))}
+          </MoviePopupSlider>
         </div>
       </div>
     </div>
